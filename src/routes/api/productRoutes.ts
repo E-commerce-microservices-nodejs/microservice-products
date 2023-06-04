3f7d697dddf0afc684b531b3f9ae0a5a02755d86
elhajjaji.riad <riad@gmail.com>
2023-06-04 15:52:49 -0400

    +

--- src/routes/api/productRoutes.ts
+++ src/routes/api/productRoutes.ts
@@ -1,7 +1,8 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
+import { Document } from 'mongoose';
+import ProductModel from '../../models/product';
 
-import { Document } from "mongoose";
-import ProductModel from "../../models/product";
+console.log(4);
 
 interface ProductFields {
   title: string;
@@ -13,22 +14,22 @@ interface ProductDocument extends ProductFields, Document {}
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   ProductModel.find()
-    .then((products) => {
+    .then(products => {
       if (products.length === 0) {
-        res.status(204).json({ message: "No products exist" });
+        res.status(204).json({ message: 'No products exist' });
       } else {
         res.json(products);
       }
     })
-    .catch((error) => {
+    .catch(error => {
       res.status(500).json({ error: error.message });
     });
 });
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
-router.get("/:id", async (req: Request, res: Response) => {
+router.get('/:id', async (req: Request, res: Response) => {
   try {
     const productId = req.params.id;
     // Find the product by ID in the database
@@ -37,37 +38,37 @@ router.get("/:id", async (req: Request, res: Response) => {
     );
     if (!product) {
       // If product is not found, return 404
-      return res.status(404).json({ message: "Product not found" });
+      return res.status(404).json({ message: 'Product not found' });
     }
     // If product is found, return it
     return res.json(product);
   } catch (error) {
     // Handle any errors that occurred during the process
-    return res.status(500).json({ message: "Internal Server Error" });
+    return res.status(500).json({ message: 'Internal Server Error' });
   }
 });
 
-router.post("/", (req: Request, res: Response) => {
+router.post('/', (req: Request, res: Response) => {
   const newProduct = new ProductModel(req.body);
   newProduct
     .save()
-    .then((product) => {
+    .then(product => {
       res.status(201);
-      res.json({ message: "Product added successfully", product });
+      res.json({ message: 'Product added successfully', product });
     })
     .catch((error: Error) => {
       res.status(500).json({ error: error.message });
     });
 });
 
-router.put("/:id", (req, res, next) => {
+router.put('/:id', (req, res, next) => {
   ProductModel.findByIdAndUpdate(req.params.id, req.body)
     .then(() => ProductModel.findById(req.params.id))
-    .then((product) => res.send(product))
+    .then(product => res.send(product))
     .catch(next);
 });
 
-router.delete("/:id", (req, res) => {
+router.delete('/:id', (req, res) => {
   ProductModel.findByIdAndRemove(req.params.id)
     .then(() => {
       res.status(404);
